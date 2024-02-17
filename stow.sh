13c173c9590a8a7f117bb3b0f16ba9481b222561
junnunkarim <24603630+junnunkarim@users.noreply.github.com>
2024-02-18 03:06:06 +0600

    made stow setup script more safe and stable

--- stow.sh
+++ stow.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
 
 # define ANSI escape codes for custom colors (everforest)
-white='\033[38;5;231m'   # #d3c6aa
-black='\033[38;5;235m'   # #272e33
+white='\033[38;5;231m'  # #d3c6aa
+black='\033[38;5;235m'  # #272e33
 gray='\033[38;5;242m'   # #859289
 red='\033[38;5;203m'    # #e67e80
 green='\033[38;5;113m'  # #a7c080
@@ -12,6 +13,7 @@ purple='\033[38;5;176m' # #d699b6
 aqua='\033[38;5;71m'    # #83c092
 orange='\033[38;5;215m' # #e69875
 reset='\033[0m'
+
 
 # arguments:
 #   a string
@@ -30,13 +32,80 @@ printc() {
     echo -ne "$2$1$reset"
   fi
 }
-
 print() {
-  printc "$1" $white
+  printc "$1" "$white"
+}
+printn() {
+  printc "$1" "$white" "n"
 }
 
-printn() {
-  printc "$1" $white "n"
+usage() {
+  if [ "$1" = 0 ] || [ "$1" = "error" ]; then
+    printc " Usage:" "$orange" "n"
+    printc "\t$0 [option]" "$yellow" "n"
+    echo
+
+    printc " Options:" "$orange" "n"
+    printc "\tsetup\t setup symlinks using stow" "$yellow" "n"
+    printc "\tremove\t remove symlinks created by stow" "$yellow" "n"
+    exit 1
+  fi
+}
+
+# arguments:
+#   a directory name (stow module name)
+#   path to find the module
+# returns:
+#   10 if successfull
+#   11 if not successfull
+take_backup() {
+  local dir_name="$1"
+  local directory="$2"
+
+  if [ -L "$directory/$dir_name" ]; then
+    printc "\`$dir_name\`" "$red"
+    print " is a symlink in "
+    printc "$directory" "$orange" "n"
+
+    print "Removing symlink of "
+    printc "$directory/$dir_name" "$orange" "n"
+
+    rm -v "$directory/$dir_name"
+
+    sleep 0.5
+
+    return 10
+  elif [ -d "$directory/$dir_name" ]; then
+    print "Directory "
+    printc "\`$dir_name\`" "$red"
+    printn " exists in $directory/ . Taking backup..."
+
+    print "Moving "
+    printc "$directory/$dir_name" "$orange"
+    print " to "
+    printc "$directory/dotfiles_backup/" "$orange" "n"
+
+    mkdir -p "$directory/dotfiles_backup"
+
+    local timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+    local new_dir_name="${dir_name}_${timestamp}"
+
+    mv -vfu "$directory/$dir_name" "$directory/dotfiles_backup/$new_dir_name"
+
+    sleep 0.5
+
+    return 10
+  else
+    print "Directory "
+    printc "\`$dir_name\`" "$red"
+    print " does not exist in "
+    printc "$directory/" "$orange"
+    printn ". Skipping backup..."
+
+    sleep 0.5
+
+    return 11
+  fi
 }
 
 # arguments:
@@ -49,28 +118,14 @@ stow_symlink() {
   stow -t ~ "$module" -v
   stow_exit_status=$?
 
-  if [ "$stow_exit_status" -eq 0 ]; then
-    echo
-    # echo "Completed setting-up stow symlinks..."
-  else
+  if [ "$stow_exit_status" != 0 ]; then
     echo
     echo "Stow setup failed."
+
     exit 1
   fi
 }
 
-usage() {
-  if [ "$1" = 0 ] || [ "$1" = "error" ]; then
-      printc " Usage:" $orange "n"
-      printc "\t$0 [option]" $yellow "n"
-      echo
-
-      printc " Options:" $orange "n"
-      printc "\tsetup\t setup symlinks using stow" $yellow "n"
-      printc "\tremove\t remove symlinks created by stow" $yellow "n"
-      exit 1
-  fi
-}
 
 # check if the number of arguments is correct
 usage $#
@@ -80,109 +135,89 @@ action="$1"
 
 # perform actions based on the parameter
 case "$action" in
-  "setup")
-    for dir in */; do
-      if [ -d "$dir" ]; then
-        dir_name=$(basename "$dir")
+"setup")
+  for dir in */; do
+    if [ -d "$dir" ]; then
+      dir_name=$(basename "$dir")
 
-        if [ -L "$HOME/.config/$dir_name" ]; then
-          printc "\`$dir_name\`" $red
-          print " is a symlink in "
-          printc "~/.config/" $orange "n"
 
-          print "Removing symlink of "
-          printc "~/.config/$dir_name/" $orange "n"
+      # TODO: make the directory finding more dynamic
+      if [ "$dir_name" = "assets" ]; then
+        take_backup "wallpaper" "$HOME/.config"
+        echo
+        take_backup "fonts" "$HOME/.local/share"
+        echo
+        take_backup ".themes" "$HOME"
+        echo
 
-          rm "$HOME/.config/$dir_name"
+        stow_symlink "$dir_name"
+      elif [ "$dir_name" = "konsole" ]; then
+        take_backup "$dir_name" "$HOME/.local/share"
 
-          sleep 0.5
-          stow_symlink "$dir_name"
+        stow_symlink "$dir_name"
+      elif [ "$dir_name" = "home_scripts" ]; then
+        take_backup ".bin" "$HOME"
 
-        elif [ -d "$HOME/.config/$dir_name" ]; then
-          print "Directory "
-          printc "\`$dir_name\`" $red
-          printn " exists in ~/.config/ . Taking backup..."
+        stow_symlink "$dir_name"
+      elif [ "$dir_name" = "utility" ]; then
+        take_backup "betterlockscreen" "$HOME/.config"
+        echo
+        take_backup "btop" "$HOME/.config"
+        echo
+        take_backup "dmenu" "$HOME/.config"
+        echo
+        take_backup "felix" "$HOME/.config"
+        echo
+        take_backup "handlr" "$HOME/.config"
+        echo
+        take_backup "helix" "$HOME/.config"
+        echo
+        take_backup "macchina" "$HOME/.config"
+        echo
+        take_backup "networkmanager-dmenu" "$HOME/.config"
+        echo
+        take_backup "qBittorrent" "$HOME/.config"
+        echo
+        take_backup "rofi" "$HOME/.config"
+        echo
+        take_backup "slock" "$HOME/.config"
+        echo
+        take_backup "zathura" "$HOME/.config"
+        echo
 
-          print "Moving "
-          printc "~/.config/$dir_name/" $orange
-          print " to "
-          printc "~/.config/dotfiles_backup/" $orange
+        stow_symlink "$dir_name"
+      else
+        take_backup "$dir_name" "$HOME/.config"
 
-          mkdir -p "$HOME/.config/dotfiles_backup"
-          cp -r "$HOME/.config/$dir_name" "$HOME/.config/dotfiles_backup"
-
-          sleep 0.5
-          stow_symlink "$dir_name"
-
-        elif [ "$dir_name" = "assets" ]; then
-          if [ -L "$HOME/.config/wallpaper" ]; then
-            printc "\`$dir_name\`" $red
-            print " is a symlink in "
-            printc "~/.config/" $orange "n"
-
-            print "Removing symlink of "
-            printc "~/.config/$dir_name/" $orange "n"
-
-            rm "$HOME/.config/$dir_name"
-
-            sleep 0.5
-            stow_symlink "$dir_name"
-
-          elif [ -d "$HOME/.config/wallpaper" ]; then
-            print "Directory "
-            printc "\`wallpaper\`" $red
-            printn " exists in ~/.config/ . Taking backup..."
-
-            print "Moving "
-            printc "~/.config/wallpaper/" $orange
-            print " to "
-            printc "~/.config/dotfiles_backup/" $orange
-
-            mkdir -p "$HOME/.config/dotfiles_backup"
-            cp -r "$HOME/.config/wallpaper" "$HOME/.config/dotfiles_backup"
-
-            sleep 0.5
-
-          fi
-
-          stow_symlink "$dir_name"
-        elif [ "$dir_name" = "home_scripts" ]; then
-        else
-          print "Directory "
-          printc "\`$dir_name\`" $red
-          print " does not exist in "
-          printc "~/.config/" $orange
-          printn ". Skipping backup..."
-          echo
-
-          sleep 0.5
-
-        fi
+        stow_symlink "$dir_name"
       fi
-    done
-    ;;
-  "remove")
-    # remove stow symlinks
-    for dir in */; do
-      if [ -d "$dir" ]; then
-        module=$(basename "$dir")
+    fi
 
-        stow -t ~ -D "$module" -v
-        stow_exit_status=$?
-
-        if [ "$stow_exit_status" != 0]; then
-          echo
-          printc "Failed to remove stow symlinks..." $red "n"
-
-          exit 1
-        fi
-      fi
-    done
-    ;;
-  *)
     echo
-    printc "Invalid action: $action" $red "n"
-    usage "error"
-    exit 1
-    ;;
+  done
+  ;;
+"remove")
+  # remove stow symlinks
+  for dir in */; do
+    if [ -d "$dir" ]; then
+      module=$(basename "$dir")
+
+      stow -t ~ -D "$module" -v
+      stow_exit_status=$?
+
+      if [ "$stow_exit_status" != 0 ]; then
+        echo
+        printc "Failed to remove stow symlinks..." "$red" "n"
+
+        exit 1
+      fi
+    fi
+  done
+  ;;
+*)
+  echo
+  printc "Invalid action: $action" "$red" "n"
+  usage "error"
+  exit 1
+  ;;
 esac
